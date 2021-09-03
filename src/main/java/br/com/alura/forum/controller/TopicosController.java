bdeed03c8586893e91c1fcedfab05fdb3d13038d
igorsrs2019 <ighor_souza@hotmail.com>
2021-09-03 19:11:18 -0300

    resolve problem with git...

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.controller;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+
+	
+	
+	
+	@RequestMapping("/topicos")
+	@ResponseBody
+	public List<TopicoDto> lista(){
+		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programacao"));
+		
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	}
+}
